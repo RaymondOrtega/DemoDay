5cb2110cade3ce2a685186048d045e2eadd32f9c
Resilient Coders 14 <resilientcoders14@Resilients-MacBook-Pro.local>
2019-08-19 15:39:22 -0400

    New Features

--- config/database.js
+++ config/database.js
@@ -1,9 +1,4 @@
-// config/database.js
-// module.exports = {
-//
-//     'url' : 'mongodb+srv://raymond:raymond@books-3bd2v.mongodb.net/test?retryWrites=true&w=majority', // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
-//     'dbName': 'WorldOfBooks'
-// };
+
 module.exports = {
     'url' : `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASSWORD}@books-3bd2v.mongodb.net/${process.env.USER_TEST}?retryWrites=true&w=majority`,
     'dbName': process.env.DB_NAME
